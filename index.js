2359d0ccbaff84214b93a17e2499edb903954c4c
Doug Loomer <loomerds@gmail.com>
2019-05-12 16:16:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
   return delete newObj.key;
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object.key;
+}
